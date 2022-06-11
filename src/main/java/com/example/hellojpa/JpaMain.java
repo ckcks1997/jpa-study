3f1f25d8f0eb7872186007e10655274560bd58d6
ckcks1997 <ckcks1997@gmail.com>
2022-06-11 14:53:52 +0900

    JPA 공부 시작
    - EntityManagerFactory, EntityManager
    - EntityTransaction
    - 영속, 준영속, 비영속 공부

---
+++ src/main/java/com/example/hellojpa/JpaMain.java
@@ -0,0 +1,24 @@
+package com.example.hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("h2db");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Member member = new Member();
+        member.setId(1112L);
+        member.setName("hello");
+        em.persist(member);
+
+        tx.commit();
+        em.close();
+        emf.close();
+    }
+}
